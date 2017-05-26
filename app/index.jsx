021f995694b9f71718765643c49b3f88a4c95268
kevalbhatt <kbhatt@apache.org>
2017-05-26 16:40:36 +0530

    react-bootstrap added and prod and dev config added

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Header } from 'common/site/Header';
+import { Button, Jumbotron } from 'react-bootstrap';
+import './styles/init.css';
 
 class App extends React.Component {
     render() {
-        return <p> Hello World </p>;
+        return (<div>
+        			<Header/>
+					<div className="container main">
+				        <Jumbotron>
+						    <h1>Hello, world!</h1>
+						    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+						    <p><Button bsStyle="primary">Learn more</Button></p>
+						</Jumbotron>
+					</div>
+				</div>);
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
-const path = require('path');
-const webpack = require('webpack');
-
-var BUILD_DIR = path.resolve(__dirname, 'public');
-var APP_DIR = path.resolve(__dirname, 'app');
-
+const path = require('path'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ENV = process.env.NODE_ENV,
+    isProd = ENV === "production" ? true : false,
+    BUILD_DIR = path.resolve(__dirname, 'public'),
+    APP_DIR = path.resolve(__dirname, 'app'),
+    NODE_MODULES = path.resolve(__dirname, 'node_modules'),
+    plugins = [];
 module.exports = {
     entry: APP_DIR + "/index.jsx",
     output: {
         path: __dirname + '/public/', //<- This path is use at build time
         filename: "bundle.js", //<- This file is created under path which we specified in output.path 
-        publicPath: "/static/" //<- This path is for dev server. you cant see this folder
+        // publicPath: "/static/" //<- This path is for dev server. you cant see this folder
+    },
+    plugins: plugins.concat([new HtmlWebpackPlugin({
+        title: 'Reaact-webpack-hrm',
+        template: 'index.ejs'
+    }), new ExtractTextPlugin({
+        // Extracting all css in one file, and file name is based on what you specified in filename
+        filename: "style.css",
+        allChunks: true
+    })]),
+    resolve: {
+        modules: [
+            APP_DIR,
+            NODE_MODULES
+        ],
+        alias: {
+            //models: path.join(__dirname, '../src/client/assets/javascripts/models')
+        },
+        extensions: ['.js', '.jsx', '.json', '.css']
     },
     module: {
         loaders: [{
@@ -20,6 +42,12 @@ module.exports = {
             // query: {
             //     presets: ["es2015", "react", "react-hmre"] <- either specify hear or in .babelrc
             // }
+        }, {
+            test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+            loader: 'url-loader'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("css-loader")
         }]
     }
 }
